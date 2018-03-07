5adee750cfe885560b2ee5b778d13e42558661d2
tetradev01 <tetradev01@tetratutorials.com>
2018-03-08 01:02:37 +0530

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cd ~/node
+cd ~/customer
 npm install
 
 # setup NODE_ENV
@@ -19,5 +19,5 @@ fi
 # add node to startup
 hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
 if [ -z "$hasRc" ]; then
-    sudo sh -c "echo 'su -l $USER -c \"cd ~/node;sh ./run.sh\"' >> /etc/rc.d/rc.local"
+    sudo sh -c "echo 'su -l $USER -c \"cd ~/customer;sh ./run.sh\"' >> /etc/rc.d/rc.local"
 fi
