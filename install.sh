d36d7e19a01929b9bdb72e71c45d858ebb67e1c9
vagrant <vagrant@vagrant.vm>
2018-03-09 20:21:56 +0000

    install files updated.

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 sudo set -e
 
-mkdir customer
-
 # update instance
 sudo yum -y update
 
 # install general libraries like Java or ImageMagick
 sudo yum -y install default-jre ImageMagick
 
+#install mysql
+sudo yum install mysql-server -y
+
+#start mysql daemon
+sudo service mysqld start
+
 # add nodejs to yum
 sudo curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-sudo yum -y install nodejs #default-jre ImageMagick
+sudo yum -y install nodejs
 
 # install pm2 module globaly
 sudo npm install -g pm2
